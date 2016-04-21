11362eaeedefc0f82459df8b7535eaacd2346382
Erasin <erasinoo@gmail.com>
2016-04-21 17:59:45 +0800

    base create

---
+++ TextConvert.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"image"
+	"image/draw"
+	"image/jpeg"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	//"github.com/unrolled/render"
+)
+
+type TextConvert struct {
+	Text string
+	rgba *image.RGBA
+	png  string
+}
+
+type textLine struct {
+	Text  string
+	Index int
+}
+
+// 字符串拆解
+func (tc *TextConvert) sliptString(lines *list.List, lineSize int) (countAll int) {
+	log.Println(tc.Text)
+	countAll = 0
+	text := strings.Replace(tc.Text, "\t", "    ", 0)
+	texts := strings.Split(text, "\n")
+
+	for _, v := range texts {
+		txt := []rune(v)
+		// 处理回车和换行
+		c := len(txt)
+		var count int
+
+		//log.Println(c/lineSize)
+
+		if n := c / lineSize; n <= 0 {
+			count = 1
+		} else {
+			count = n + 1
+		}
+
+		//log.Println(c, count)
+
+		for j := 0; j < count; j++ {
+			start := j * lineSize
+			end := (j + 1) * lineSize
+			if j == count-1 {
+				//log.Println(start, end,countAll+j)
+				lines.PushBack(textLine{string(txt[start:]), countAll + j})
+				// t.Lines[j] = string(txt[start:])
+			} else {
+				//log.Println(start, end,countAll+j)
+				lines.PushBack(textLine{string(txt[start:end]), countAll + j})
+				// t.Lines[j] = string(txt[start:end])
+			}
+		}
+
+		countAll += count
+		//countAll += 1
+		//lines.PushBack(textLine{"",countAll})
+	}
+
+	return
+}
+
+func (tc *TextConvert) Write(p []byte) (n int, err error) {
+	tc.Text += string(p)
+	return 0, nil
+}
+
+func (tc *TextConvert) doImg() {
+	var fontTTF = "/Library/Fonts/华文仿宋.ttf"
+	var size = 25.0
+	var dx = 500
+	var dy = 300
+
+	lineSize := int(dx / int(size))
+
+	// 计算行数以及拆解字符串
+	lines := list.New()
+	tl := tc.sliptString(lines, lineSize)
+	dy = tl * int(size)
+
+	fontb, err := ioutil.ReadFile(fontTTF)
+	if err != nil {
+		log.Println(err)
+	}
+	fontf, err := truetype.Parse(fontb)
+	if err != nil {
+		log.Println(err)
+	}
+	fg, bg := image.Black, image.White
+	tc.rgba = image.NewRGBA(image.Rect(0, 0, dx, dy))
+	draw.Draw(tc.rgba, tc.rgba.Bounds(), bg, image.ZP, draw.Src)
+	c := freetype.NewContext()
+	c.SetDPI(72)
+	c.SetFont(fontf)
+	c.SetFontSize(size)
+	c.SetClip(tc.rgba.Bounds())
+	c.SetDst(tc.rgba)
+	c.SetSrc(fg)
+
+	opts := truetype.Options{}
+	opts.Size = size
+	// face := truetype.NewFace(f, &opts)
+	// faceWidth,ok := face.GlyphAdvance(rune("中"))
+	intSize := int(size)
+
+	for txt := lines.Front(); txt != nil; txt = txt.Next() {
+		t := txt.Value.(textLine)
+		pt := freetype.Pt(0, (t.Index+1)*intSize)
+		c.DrawString(t.Text, pt)
+	}
+
+	return
+}
+
+func (tc *TextConvert) writeTo(w http.ResponseWriter) {
+	//bf := bufio.NewWriter(w)
+	//err := png.Encode(w, tc.rgba)
+	err := jpeg.Encode(w, tc.rgba, &jpeg.Options{80})
+
+	//err = png.Encode(bf, tc.rgba)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func (tc *TextConvert) saveImg() {
+	outFile, _ := os.Create("demo2.png")
+	defer outFile.Close()
+	bf := bufio.NewWriter(outFile)
+	err := png.Encode(bf, tc.rgba)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	err = bf.Flush()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"html/template"
+
+	"github.com/lunny/tango"
+	//"github.com/unrolled/render"
+)
+
+func main() {
+
+	tg := tango.Classic()
+
+	tg.Get("/:to/:from/:date", new(TxtImgCon))
+
+	//graceful.Run(":9092", 10*time.Second, st)
+	tg.Run(":9092")
+}
+
+type TxtImgCon struct {
+	tango.Params
+	//tango.GZip
+	tango.Ctx
+}
+
+//func (t TxtImgCon) Get(w http.ResponseWriter, req *http.Request) {
+func (t TxtImgCon) Get() {
+
+	StrTo := t.Params.Get(":to")
+	StrFrom := t.Params.Get(":from")
+	StrDate := t.Params.Get(":date")
+	//w.Write([]byte(Name))
+	//t.Ctx.ResponseWriter.Write([]byte(Name))
+	//return
+
+	var tplMain = `尊敬的 {{.To}}:
+
+	这里是您的邀请函,你可以凭借此文件来我公司进行商谈.
+
+									重要: {{.From}}
+									{{.Date}}
+
+`
+	tc := &TextConvert{}
+
+	tpMain, _ := template.New("main").Parse(tplMain)
+	tpMain.Execute(tc, map[string]string{"To": StrTo, "From": StrFrom, "Date": StrDate})
+
+	tc.doImg()
+	tc.writeTo(t.Ctx.ResponseWriter)
+	//return
+
+}
